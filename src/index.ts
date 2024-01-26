33dd1eb2bec1174ac7b499c12d4e11b3a490e2d1
rainbowatcher <rainbow-w@qq.com>
2024-01-26 09:29:00 +0800

    feat: use vitest instead of uvu

--- src/index.ts
+++ src/index.ts
@@ -4,17 +4,31 @@ import { nodeHTTPRequestHandler } from "@trpc/server/adapters/node-http"
 import type { NodeHTTPCreateContextFnOptions, NodeHTTPHandlerOptions } from "@trpc/server/adapters/node-http"
 import type { AnyRouter } from "@trpc/server"
 
-export type CreateH3ContextOptions = NodeHTTPCreateContextFnOptions<IncomingMessage, ServerResponse>
+export type CreateH3ContextOptions = NodeHTTPCreateContextFnOptions<IncomingMessage, ServerResponse<IncomingMessage>>
+
+export type AdditionalMiddlewareOpts = { prefix?: `/${string}` };
+export type CreateH3MiddlewareOptions<TRouter extends AnyRouter> = NodeHTTPHandlerOptions<
+  TRouter,
+  IncomingMessage,
+  ServerResponse<IncomingMessage>
+> &
+  AdditionalMiddlewareOpts;
+
 export function createH3Middleware<TRouter extends AnyRouter>(
-  opts: NodeHTTPHandlerOptions<TRouter, IncomingMessage, ServerResponse>,
+  opts: CreateH3MiddlewareOptions<TRouter>,
 ): h3.EventHandler {
   return h3.defineEventHandler(async (e) => {
     const endpoint = h3.getRequestURL(e).pathname.slice(1)
+    const { prefix, router, createContext } = opts
+    const { req, res } = e.node
+
+    if (prefix && !h3.getRequestURL(e).pathname.startsWith(prefix)) return e;
 
     await nodeHTTPRequestHandler({
-      ...opts,
-      req: e.node.req,
-      res: e.node.res,
+      router,
+      createContext,
+      req,
+      res,
       path: endpoint,
     })
   })
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,38 +1,42 @@
-import { createServer } from "node:http"
-import { test } from "uvu"
-import * as assert from "uvu/assert"
-import { initTRPC } from "@trpc/server"
+import { Server, createServer } from "node:http"
+import { it, expect, beforeAll, afterAll } from "vitest"
+import { initTRPC, AnyRouter } from "@trpc/server"
+import { createTRPCProxyClient, httpBatchLink } from "@trpc/client"
 import z from "zod"
 import { createApp, toNodeListener } from "h3"
-import { createTRPCProxyClient, httpBatchLink } from "@trpc/client"
 import { createH3Middleware } from "../src"
 
-const { router, procedure } = initTRPC.create()
+let server: Server, client: any;
 
-const appRouter = router({
-  hello: procedure.input(z.string()).query((opts) => {
-    return `hello ${opts.input}`
-  }),
-})
-
-type AppRouter = typeof appRouter
-
-const app = createApp()
-app.use("/trpc", createH3Middleware({ router: appRouter }))
-const server = createServer(toNodeListener(app)).listen(3000)
-
-const client = createTRPCProxyClient<AppRouter>({
-  links: [
-    httpBatchLink({
-      url: "http://localhost:3000/trpc",
+beforeAll(() => {
+  const { router, procedure } = initTRPC.create()
+  
+  const appRouter = router({
+    hello: procedure.input(z.string()).query((opts) => {
+      return `hello ${opts.input}`
     }),
-  ],
+  })
+  
+  type AppRouter = typeof appRouter
+  
+  const app = createApp()
+  app.use("/trpc", createH3Middleware({ router: appRouter }))
+  server = createServer(toNodeListener(app)).listen(3000)
+  
+  client = createTRPCProxyClient<AppRouter>({
+    links: [
+      httpBatchLink({
+        url: "http://localhost:3000/trpc",
+      }),
+    ],
+  })
 })
 
-test("test hello", async () => {
-  const resp = await client.hello.query("world")
-  assert.equal(resp, "hello world")
+afterAll(() => {
   server.close()
 })
 
-test.run()
+it("test hello", async () => {
+  const resp = await client.hello.query("world")
+  expect(resp).toEqual("hello world")
+})
